8e432d72ad22b63429e1bd3afa9553f25b97ef41
F. Gold <fgold@vmware.com>
2022-03-16 13:10:30 -0700

    Concurrent pluginplay
    Signed-off-by: F. Gold <fgold@vmware.com>

--- plugin-play/cmd/plugin/pluginplay/hello-world.go
+++ plugin-play/cmd/plugin/pluginplay/hello-world.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"time"
 
 	"github.com/spf13/cobra"
 )
@@ -17,5 +18,31 @@ var HelloWorldCmd = &cobra.Command{
 
 func helloWorld(cmd *cobra.Command, _ []string) error {
 	fmt.Println("Hello, World.")
+
+	ticker := time.NewTicker(2500 * time.Millisecond)
+	done := make(chan bool)
+
+	go func() {
+		var i int
+		for {
+			select {
+			case <-done:
+				return
+			case <-ticker.C:
+				if i%2 == 0 {
+					fmt.Println(i, "tick")
+				} else {
+					fmt.Println(i, "tock")
+				}
+				i++
+			}
+		}
+	}()
+
+	time.Sleep(1 * time.Minute)
+	ticker.Stop()
+	done <- true
+	fmt.Println("Tick-tock stopped")
+
 	return nil
 }
